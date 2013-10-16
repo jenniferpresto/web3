9fe509c009bc25c77d8277f73df1ec8f2a395859
Jennifer Presto <SandlapperNYC@Macintosh-0023dffe3f3b.local>
2013-10-16 06:40:39 -0400

    adding form for entry to story

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -17,3 +17,12 @@ $('header nav').on('click', 'a', function(event){
 /************
 Variables for cartodb
 *************/
+
+var cartodb_accountname = 'sandlappernyc';
+var cartodo_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
+var table_name = 'narrativetable';
+
+/************
+Inserting row into cartodb table
+*************/
+
