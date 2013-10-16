d3c2809701fa8a10f939b5ec421b2cfa5b0a8fee
Jennifer Presto <SandlapperNYC@Macintosh-0023dffe3f3b.local>
2013-10-16 07:40:29 -0400

    successfully generating URL for writing to table

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -19,10 +19,45 @@ Variables for cartodb
 *************/
 
 var cartodb_accountname = 'sandlappernyc';
-var cartodo_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
+var writeUrl = 'http://sandlappernyc.cartodb.com/api/v2/sql?q=';
+var api_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
 var table_name = 'narrativetable';
 
 /************
 Inserting row into cartodb table
 *************/
 
+/* this works:
+INSERT INTO narrativetable (author, narrativetext) VALUES ( 'testAuthor', 'testing the text');
+
+Example write from cartodb:
+http://sandlappernyc.cartodb.com/api/v2/sql?q=INSERT INTO table_name (the_geom, observation) VALUES (ST_GeomFromText(’POINT(-71.2 42.5)’, 4326),'rare bird spotted')&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101
+
+*/
+
+
+
+function addStoryEntry (author, content) {
+	var sqlInsert = "INSERT INTO " + table_name + " (author, narrativetext) VALUES ( '" + author + "', '" + content + "');"
+	var writeCommand = writeUrl + sqlInsert + api_key;
+	
+	// console.log(sqlInsert);
+	console.log(writeCommand);
+
+}
+
+/************
+Click event for addStory button
+*************/
+
+$('#enterStory button').click (function (event) {
+
+	// stop form from sending/refreshing page
+	event.preventDefault(event);
+
+	// create variables from form
+	var author = $('#author').val();
+	var content = $('#content').val();
+
+	addStoryEntry(author, content);
+})
