6c9a079ce449aa1a8d66e51c6bcd5226d7d88b80
Jennifer Presto <presj521@newschool.edu>
2013-10-16 21:01:41 -0400

    getting a successful query for all the details for one story at a time

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -15,51 +15,6 @@ $('header nav').on('click', 'a', function(event){
 })
 
 /************
-Variables for cartodb
-*************/
-
-var cartodb_accountname = 'sandlappernyc';
-var writeUrl = 'http://sandlappernyc.cartodb.com/api/v2/sql?q=';
-var carto_api_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
-var table_name = 'narrativetable';
-
-/************
-Inserting row into cartodb table
-*************/
-
-/* this works:
-INSERT INTO narrativetable (author, narrativetext) VALUES ( 'testAuthor', 'testing the text');
-
-Example write from cartodb:
-http://sandlappernyc.cartodb.com/api/v2/sql?q=INSERT INTO table_name (the_geom, observation) VALUES (ST_GeomFromText(’POINT(-71.2 42.5)’, 4326),'rare bird spotted')&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101
-
-*/
-
-
-
-function addStoryEntry (author, content) {
-	var sqlInsert = "INSERT INTO " + table_name + " (author, narrativetext) VALUES ( '" + author + "', '" + content + "');"
-	var writeCommand = writeUrl + sqlInsert + carto_api_key;
-	
-	// console.log(sqlInsert);
-	console.log(writeCommand);
-
-	$.getJSON(writeCommand, function(data) {
-		console.log(data);
-	})
-	.success(function(response) {
-		console.log('table successfully updated');
-		console.log(response);
-	})
-	.error(function () {
-		console.log('Error');
-	})
-	.complete(function() { 
-		console.log('complete');
-	});
-}
-
-/************
 Click event for addToStory button
 *************/
 
@@ -88,6 +43,80 @@ $('#newStory').click (function (event) { // could also say button#newStory
 	getRandomImage();
 
 })
+
+/************
+General variables for cartodb
+*************/
+
+var cartodb_accountname = 'sandlappernyc';
+var writeUrl = 'http://sandlappernyc.cartodb.com/api/v2/sql?q=';
+var carto_api_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
+var table_name = 'narrativetable';
+var cartoCommand;
+
+/************
+Querying cartodb for story details
+*************/
+
+/*
+This works:
+SELECT * FROM narrativetable WHERE (storytitle = 1) ORDER BY (created_at) ASC
+*/
+
+function queryStoryDetails (titleNumber) {
+	var sqlStoryQuery = "SELECT * FROM " + table_name + " WHERE (storytitle = " + titleNumber + ") ORDER BY (created_at) ASC";
+	cartoCommand = writeUrl + sqlStoryQuery + carto_api_key;
+
+	var storyOutput = [];
+	var templateA = $('.details li.template'); // template in HTML
+	var query_count; // total returned
+
+	$.getJSON(cartoCommand, function(detailData) {
+		query_count = detailData.rows.length;
+		console.log(detailData.rows);
+		console.log(query_count);
+		storyOutput = detailData.rows;
+	})
+}
+
+queryStoryDetails(2);
+
+/************
+Inserting row into cartodb table
+*************/
+
+/* this works:
+INSERT INTO narrativetable (author, narrativetext) VALUES ( 'testAuthor', 'testing the text');
+
+Example write from cartodb:
+http://sandlappernyc.cartodb.com/api/v2/sql?q=INSERT INTO table_name (the_geom, observation) VALUES (ST_GeomFromText(’POINT(-71.2 42.5)’, 4326),'rare bird spotted')&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101
+
+*/
+
+
+
+function addStoryEntry (author, content) {
+	var sqlStoryEntry = "INSERT INTO " + table_name + " (author, narrativetext) VALUES ( '" + author + "', '" + content + "');"
+	writeCommand = writeUrl + sqlStoryEntry + carto_api_key;
+	
+	// console.log(sqlInsert);
+	console.log(writeCommand);
+
+	$.getJSON(writeCommand, function(data) {
+		console.log(data);
+	})
+	.success(function(response) {
+		console.log('table successfully updated');
+		console.log(response);
+	})
+	.error(function () {
+		console.log('Error');
+	})
+	.complete(function() { 
+		console.log('complete');
+	});
+}
+
 
 /************
 Variables for imgur API
