2246bfd534da29d7f13910eebe638c8009ebcf23
Jennifer Presto <presj521@newschool.edu>
2013-10-18 19:01:03 -0400

    trying to get story to refresh when you add a new line

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -1,4 +1,19 @@
-var currentTitle = 1;
+/************
+Global variables
+*************/
+var currentTitleNumber = 3; // starts at whatever the latest story is
+var currentTitleText;
+var currentTitleImage;
+var allTitles = []; // will hold all story titles
+
+// for CartoDB ---------------
+var cartodb_accountname = 'sandlappernyc';
+var cartoUrl = 'http://sandlappernyc.cartodb.com/api/v2/sql?q=';
+var carto_api_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
+var table_name = 'narrativetable';
+var title_table_name = 'titletable';
+var cartoCommand;
+
 
 /************
 Page navigation
@@ -47,32 +62,57 @@ $('#newStory').click (function (event) { // could also say button#newStory
 })
 
 /************
-General variables for cartodb
-*************/
+Querying cartodb for all story titles
 
-var cartodb_accountname = 'sandlappernyc';
-var writeUrl = 'http://sandlappernyc.cartodb.com/api/v2/sql?q=';
-var carto_api_key = '&api_key=b3c3e1286652256b3974e4e06e7c6811f5f16101';
-var table_name = 'narrativetable';
-var title_table_name = 'titletable';
-var cartoCommand;
-
-/************
-Querying cartodb for story title
-
-This works:
+This works for querying just one title:
 SELECT * FROM titletable WHERE (titlenumber = 1)
 *************/
 
-function queryStoryTitle (titleNumber) {
-	var sqlTitleQuery = "SELECT * FROM " + title_table_name + " WHERE (titlenumber = " + currentTitle + ")";
-	cartoCommand = writeUrl + sqlTitleQuery + carto_api_key;
+function queryStoryTitles () {
+	// var sqlTitleQuery = "SELECT * FROM " + title_table_name + " WHERE (titlenumber = " + currentTitle + ")";
+	var sqlTitleQuery = "SELECT * FROM " + title_table_name;
+	cartoCommand = cartoUrl + sqlTitleQuery + carto_api_key;
+	var allTitles = [];
+	var templateTitles = $('#titleListAll li.template'); // template in HTML
+	var numTitles;
 
-	var titleOutput = [];
-	console.log("titleOutput", titleOutput);
+	$.getJSON(cartoCommand, function(titleData) {
+		console.log("titleData", titleData);
+		numTitles = titleData.rows.length;
+		for (var i = 0; i < numTitles; i++) {
+			// create line for each entry
+			var template = templateTitles.clone();
+			template.removeClass('template');
+			template.addClass('titleList');
+			// $(template).html(titleData.rows[i].title);
+			template.find('.storyTitle').html(titleData.rows[i].title);
+			if (titleData.rows[i].inprogress) {
+				template.find('.progress').html('<i>story in progress</i>');
+			}
+
+			allTitles.push(template);
+
+			// save title text to global variable
+			if (titleData.rows.titlenumber == currentTitleNumber) {
+				currentTitleText = titleData.rows.title;
+				currentTitleImage = titleData.rows.imageurl;
+			}
+		}
+	})
+	.success(function() {
+		// console.log("JSON success in getting title details");
+		if (numTitles > 0) {
+			$('#titleListAll ul').append(allTitles);
+			$('#titleListAll ul').append("<section class='clearfix'></section>");
+		} else {
+			console.log('no titles yet');
+		}
+	})
+	.error (function() {
+
+	})
+	.complete (function() {});
 }
-
-queryStoryTitle(currentTitle);
 
 /************
 Querying cartodb for story details
@@ -84,16 +124,22 @@ SELECT * FROM narrativetable WHERE (storytitle = 1) ORDER BY (created_at) ASC
 
 function queryStoryDetails (titleNumber) {
 	var sqlStoryQuery = "SELECT * FROM " + table_name + " WHERE (storytitle = " + titleNumber + ") ORDER BY (created_at) ASC";
-	cartoCommand = writeUrl + sqlStoryQuery + carto_api_key;
+	cartoCommand = cartoUrl + sqlStoryQuery + carto_api_key;
 
 	var storyOutput = [];
 	var templateA = $('.details li.template'); // template in HTML
-	console.log ("this is templateA: ", templateA);
+	// console.log ("this is templateA: ", templateA);
 	var query_count; // total returned
+
+	// remove all lines from the story detail page (will put back with getJSON function)
+	$('.priorDetail').remove();
+	// $('.credit').remove();
+	// // put the template back so it can be replaced
+	// $('.details ul').append('<li class="template"></li>');
 
 	$.getJSON(cartoCommand, function(detailData) {
 		query_count = detailData.rows.length;
-		console.log(detailData.rows);
+		// console.log(detailData.rows);
 
 		// how would I use $.each for this and access the appropriate text for each one?
 		// $.each(detailData.rows, function () {
@@ -113,11 +159,11 @@ function queryStoryDetails (titleNumber) {
 
 			storyOutput.push(template);
 		}
-		console.log("storyOutput length: " + storyOutput.length);
-		console.log("storyOutput is this: ", storyOutput);
+		// console.log("storyOutput length: " + storyOutput.length);
+		// console.log("storyOutput is this: ", storyOutput);
 	})
 	.success(function() {
-		console.log('getJSON success for storydetails');
+		// console.log('getJSON success for storydetails');
 		if (query_count > 0) {
 			$('.details ul').append(storyOutput);
 			$('.details ul').append("<section class='clearfix'></section>");
@@ -130,8 +176,6 @@ function queryStoryDetails (titleNumber) {
 	})
 	.complete(function() {});
 };
-
-queryStoryDetails(currentTitle);
 
 /************
 Inserting row into cartodb table
@@ -148,8 +192,8 @@ http://sandlappernyc.cartodb.com/api/v2/sql?q=INSERT INTO table_name (the_geom, 
 
 
 function addStoryEntry (author, content) {
-	var sqlStoryEntry = "INSERT INTO " + table_name + " (author, narrativetext) VALUES ( '" + author + "', '" + content + "');"
-	writeCommand = writeUrl + sqlStoryEntry + carto_api_key;
+	var sqlStoryEntry = "INSERT INTO " + table_name + " (author, narrativetext, storytitle) VALUES ( '" + author + "', '" + content + "', '" + currentTitleNumber + "');"
+	writeCommand = cartoUrl + sqlStoryEntry + carto_api_key;
 	
 	// console.log(sqlInsert);
 	console.log(writeCommand);
@@ -167,6 +211,9 @@ function addStoryEntry (author, content) {
 	.complete(function() { 
 		console.log('complete');
 	});
+
+	// call function to update page
+	queryStoryDetails(currentTitleNumber);
 }
 
 
@@ -199,15 +246,15 @@ function getRandomImage () {
 				}
 			}
 
-			console.log("length of animatedGifs: " + animatedGifs.length);
-			console.log("length of stillImages: " + stillImages.length);
+			// console.log("length of animatedGifs: " + animatedGifs.length);
+			// console.log("length of stillImages: " + stillImages.length);
 
 			var imageUrl;
 
 			// prioritize animated GIFs over other possibilities
 			if (animatedGifs.length > 0) {
 				var randomNumber = Math.floor(Math.random() * animatedGifs.length);
-				console.log(randomNumber);
+				// console.log(randomNumber);
 				imageUrl = animatedGifs[randomNumber];
 			} else {
 				randomNumber = Math.floor(Math.random() * stillImages.length);
@@ -217,3 +264,10 @@ function getRandomImage () {
 		}
 	})
 }
+
+/*********************************************
+Calling certain functions right away
+*********************************************/
+
+queryStoryTitles();
+queryStoryDetails(currentTitleNumber);
