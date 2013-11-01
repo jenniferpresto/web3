ec0f8f3212ba116594aefe019a362002d4ee15fb
Jennifer Presto <sandlappernyc@yahoo.com>
2013-11-01 11:47:54 -0400

    date is now prettier

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -247,7 +247,87 @@ function getTitleMetrics(numTitles) {
 			}
 		}
 
-		console.log(titlesArray);
+		// turn the date into a more readable, less ugly format
+		for ( var i = 0; i < numTitles; i++ ) {
+			var d = Date.parse(titlesArray[i].updated);
+			var readable = new Date(d);
+			var day = readable.getDay();
+			switch (day) {
+				case 0:
+				day = "Sunday";
+				break;
+				case 1:
+				day = "Monday";
+				break;
+				case 2:
+				day = "Tuesday";
+				break;
+				case 3:
+				day = "Wednesday";
+				break;
+				case 4:
+				day = "Thursday";
+				break;
+				case 5:
+				day = "Friday";
+				break;
+				case 6:
+				day = "Saturday";
+				break;
+			}
+			var month = readable.getMonth();
+			switch (month) {
+				case 0:
+				month = "January";
+				break;
+				case 1:
+				month = "February";
+				break;
+				case 2:
+				month = "March";
+				break;
+				case 3:
+				month = "April";
+				break;
+				case 4:
+				month = "May";
+				break;
+				case 5:
+				month = "June";
+				break;
+				case 6:
+				month = "July";
+				break;
+				case 7:
+				month = "August";
+				break;
+				case 8:
+				month = "September";
+				break;
+				case 9:
+				month = "October";
+				break;
+				case 10:
+				month = "November";
+				break;
+				case 11:
+				month = "December";
+				break;
+			}
+			var date = readable.getDate();
+			var year = readable.getFullYear();
+			var time = readable.toLocaleTimeString();
+
+			var dateString;
+			if (titlesArray[i].updated == '0000000000000') {
+				dateString = 'no entries yet'
+			} else {
+				dateString = 'last updated ' + month + ' ' + date + ', ' + year + '  ' + time;
+			}
+
+			console.log(dateString);
+		}
+
 	})
 
 }
