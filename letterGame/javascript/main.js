51066dba253e5a78e765fc801ff98184f2913048
Jennifer Presto <presj521@newschool.edu>
2013-11-24 17:10:29 -0500

    started adding stuff for mouse interaction

--- letterGame/javascript/main.js
+++ letterGame/javascript/main.js
@@ -33,17 +33,19 @@ window.onload = function () {
     // }
 
     // var Box2DCode = function () { 
-	var   b2Vec2 = Box2D.Common.Math.b2Vec2
-      , b2BodyDef = Box2D.Dynamics.b2BodyDef
-      , b2Body = Box2D.Dynamics.b2Body
-      , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-      , b2Fixture = Box2D.Dynamics.b2Fixture
-      , b2World = Box2D.Dynamics.b2World
-      , b2MassData = Box2D.Collision.Shapes.b2MassData
-      , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-      , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-      , b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-      ;
+	var     b2Vec2 = Box2D.Common.Math.b2Vec2
+        ,   b2AABB = Box2D.Collision.b2AABB
+        ,   b2BodyDef = Box2D.Dynamics.b2BodyDef
+        ,   b2Body = Box2D.Dynamics.b2Body
+        ,   b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+        ,   b2Fixture = Box2D.Dynamics.b2Fixture
+        ,   b2World = Box2D.Dynamics.b2World
+        ,   b2MassData = Box2D.Collision.Shapes.b2MassData
+        ,   b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+        ,   b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+        ,   b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+        ,   b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef
+        ;
 
     // Box2D world
     var world = new b2World (
@@ -141,11 +143,7 @@ window.onload = function () {
 
     // fire it up with one call
     requestAnimFrame(update);
-
-
-
 	// }
 
     // loadScript('javascript/Box2dWeb-2.1.a.3.min.js', Box2DCode);
-
 }
